0056dc9ab2a3516e06ff55b7a0eec9dac7c10841
Nadirah Jama <Nadirahjama@gmail.com>
2020-12-26 21:51:31 -0600

    added markdown and other functions

---
+++ generatemarkdownfile.js
@@ -0,0 +1,36 @@
+// function to generate markdown for README
+function generateMarkdown(data) {
+    return `# ${data.title}
+     <h1 align="center">${data.title} </h1>
+    
+    ![badge](https://img.shields.io/badge/license-${data.license}-green)<br />
+    ## Description
+     ${data.description}
+    ## Table of Contents
+    - [Description](#description)
+    - [Installation](#installation)
+    - [Usage](#usage)
+    - [License](#license)
+    - [Contributing](#contributing)
+    - [Tests](#tests)
+    - [Questions](#questions)
+    ## Installation
+    ${data.installation}
+    ## Usage
+    ${data.usage}
+    ## License
+    ![badge](https://img.shields.io/badge/license-${data.license}-green)
+    <br />
+    This application is covered by the ${data.license} license. 
+    ## Contributions
+    ${data.contributions}
+    ## Tests
+    ${data.tests}
+    ## Questions
+    You can find me on GitHub: [${data.github}](https://github.com/${data.github})<br />
+    <br />
+    ✉️ Email me: ${data.email}
+        `;
+  }
+  
+  module.exports = generateMarkdown;
--- index.js
+++ index.js
@@ -1,42 +1,48 @@
 const inquirer = require('inquirer');
-const fs = require('fs');
+const fs = require("fs");
+const util = require("util");
 
-inquirer
-    .prompt([
+// Internal modules
+const generateMarkdown = require("./generatemarkdownfile");
+
+
+const promptUser = 
+
+    [
         {
             type: "input",
             name: "Title",
             message: "Name of project"
-        }, 
+        },
         {
             type: "input",
             name: "description",
             message: "Describe this project",
-           
+
         },
         {
             type: "input",
             name: "Installation instruction",
             message: "Installations",
-           
+
         },
         {
             type: "input",
             name: "Usage information",
             message: "Usage",
-           
+
         },
         {
             type: "input",
             name: "Contribution guidelines",
             message: "Contribution",
-           
+
         },
         {
             type: "input",
             name: "Test instruction",
             message: "Test",
-           
+
         },
         {
             type: "checkbox",
@@ -45,27 +51,40 @@ inquirer
             choices: [
 
                 {
-                    name: "MIT", 
+                    name: "MIT",
                 },
                 {
-                    name:"ISC"
-                }, 
+                    name: "ISC"
+                },
                 {
                     name: "Apache"
                 },
-                { 
-                    name:"Unlicense"
+                {
+                    name: "Unlicense"
                 }
             ]
-          
+
         }
-    ])
-    .then(response => {
-        console.log("correct!", response)
-        fs.writeFile("readme.json", JSON.stringify(response, null, 4), {}, () => console.log("file written!"))
+    ]
 
-    })
-    .catch(err => {
-        console.log("try again", err)
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        if (err) {
+            return console.log(err);
+        }
+    });
+}
 
-    })
+function init() {
+    inquirer.prompt(promptUser).then((data) => {
+        let markDown = generateMarkdown(data);
+        let license = "";
+
+        console.log(markDown);
+        writeToFile("README.md", markDown);
+        // console.log("Success! Your README.md file has been generated");
+    });
+}
+
+// function call to initialize program
+init();
